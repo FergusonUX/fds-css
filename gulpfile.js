6c75c7e49f53bdcc0111cf606b1de4d0859f8714
Matthew Evert <hello.mevert@gmail.com>
2017-11-14 07:21:54 -0500

    Added tests worksheet

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+var http = require('http');
+var st = require('st');
+
+gulp.task('compile-sass', function() {
+    gulp.src('fds.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'))
+        .pipe(livereload());;
+});
+
+// gulp.task('watch2', function() {
+    // livereload.listen({ start: true, port: 3000, basePath: 'tests', reloadPage: 'test.html' });
+    // gulp.watch('./sass/**/*.sass', ['compile-sass']);
+// });
+
+gulp.task('watch', ['server'], function() {
+  livereload.listen({ basePath: 'tests' });
+  gulp.watch('./sass/**/*.sass', ['compile-sass']);
+});
+
+gulp.task('server', function(done) {
+  http.createServer(
+    st({ path: __dirname + '/tests', index: 'test.html', cache: false })
+  ).listen(8080, done);
+});

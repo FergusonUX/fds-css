757f771a728e4401458764d2e2746e645e84fe94
Matthew Evert <hello.mevert@gmail.com>
2018-02-06 13:25:40 -0500

    Removed line breaks in declared-variables

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,31 @@ var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
 var http = require('http');
 var st = require('st');
+var rename = require('gulp-rename');
 
 gulp.task('compile-sass', function() {
+  var pkg = require('./package.json')
     gulp.src('fds.sass')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css/'))
-        .pipe(livereload());;
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Note: outputStyle in gulp-sass has four options: nested, expanded, compact, compressed
+        .pipe(rename('fds.min.css'))
+        .pipe(gulp.dest('css/'+pkg.version+'/'))
+        .pipe(livereload());
+    gulp.src('fds.sass')
+        .pipe(sass().on('error', sass.logError)) // Note: outputStyle in gulp-sass has four options: nested, expanded, compact, compressed
+        .pipe(rename('fds.css'))
+        .pipe(gulp.dest('css/'+pkg.version+'/'))
+        .pipe(livereload());
+    gulp.src('fds.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Note: outputStyle in gulp-sass has four options: nested, expanded, compact, compressed
+        .pipe(rename('fds.min.css'))
+        .pipe(gulp.dest('css/latest/'))
+        .pipe(livereload());
+    gulp.src('fds.sass')
+        .pipe(sass().on('error', sass.logError)) // Note: outputStyle in gulp-sass has four options: nested, expanded, compact, compressed
+        .pipe(rename('fds.css'))
+        .pipe(gulp.dest('css/latest/'))
+        .pipe(livereload());
+
 });
 
 // gulp.task('watch2', function() {
